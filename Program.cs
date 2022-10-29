c598c11e1f34ef8c91ecffdf08c756b22e614efe
Your Name <you@example.com>
2022-10-29 21:07:45 +0400

    решили задачу 1 без цикла

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+Console.WriteLine("Введите натуральное число N");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int stepen = int.Parse(Console.ReadLine());
+
+double F(int A, int B){
+    double res = Math.Pow(A, B);
+    return res;
+}
+Console.WriteLine();
+Console.WriteLine($"{N} в степени {stepen} = {F(N,stepen)}");
+Console.WriteLine();
