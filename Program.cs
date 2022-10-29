39bda81a6beea48d28f45c7868c22e9b3db48b6f
Your Name <you@example.com>
2022-10-29 22:26:20 +0400

    решили задачу 1 с циклом

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+﻿/* программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B
 Console.WriteLine("Введите натуральное число N");
 int N = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите степень");
@@ -7,6 +7,25 @@ int stepen = int.Parse(Console.ReadLine());
 double F(int A, int B){
     double res = Math.Pow(A, B);
     return res;
+}
+Console.WriteLine();
+Console.WriteLine($"{N} в степени {stepen} = {F(N,stepen)}");
+Console.WriteLine();
+*/
+
+// 1. цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+Console.WriteLine("Введите натуральное число N");
+double N = double.Parse (Console.ReadLine());
+Console.WriteLine("Введите степень");
+double stepen = double.Parse(Console.ReadLine());
+
+double F(double A, double B)
+{   double res=1;
+    for (int count=0; count<Math.Abs(B); count++){
+    if (B>0)  res = res*A;
+    if (B<0)  res = res*(1/A);}
+return res;
 }
 Console.WriteLine();
 Console.WriteLine($"{N} в степени {stepen} = {F(N,stepen)}");
