3f87d5e894a37d3b0c70497fe06686522a296255
Your Name <you@example.com>
2022-10-29 22:43:47 +0400

    решили задачу 2 - сумма цифр в числе

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ Console.WriteLine($"{N} в степени {stepen} = {F(N,stepen)}");
 Console.WriteLine();
 */
 
-// 1. цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+/* 1. цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
 
-Console.WriteLine("Введите натуральное число N");
+Console.WriteLine("Введите число N");
 double N = double.Parse (Console.ReadLine());
 Console.WriteLine("Введите степень");
 double stepen = double.Parse(Console.ReadLine());
@@ -30,3 +30,23 @@ return res;
 Console.WriteLine();
 Console.WriteLine($"{N} в степени {stepen} = {F(N,stepen)}");
 Console.WriteLine();
+*/
+
+//программа, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите натуральное число N");
+int N = int.Parse (Console.ReadLine());
+
+int Sum(int num){
+int sum=0;
+int temp=0;
+int ostatok=num;
+    while (ostatok != 0) {
+        temp = ostatok%10;
+        sum = sum + temp;
+        ostatok = ostatok/10;
+    }
+return sum;
+}
+Console.WriteLine($"Сумма цифр в числе = {Sum(N)}");
+
