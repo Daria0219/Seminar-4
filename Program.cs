029151664f1861c53996b3b5bbad1d94ce009ba8
Your Name <you@example.com>
2022-10-30 15:08:02 +0400

    черновик

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ Console.WriteLine($"{N} в степени {stepen} = {F(N,stepen)}");
 Console.WriteLine();
 */
 
-//программа, которая принимает на вход число и выдаёт сумму цифр в числе
+/* 2. программа, которая принимает на вход число и выдаёт сумму цифр в числе
 
 Console.WriteLine("Введите натуральное число N");
 int N = int.Parse (Console.ReadLine());
@@ -49,4 +49,95 @@ int ostatok=num;
 return sum;
 }
 Console.WriteLine($"Сумма цифр в числе = {Sum(N)}");
+*/
 
+/* программа, которая задаёт массив из 8 элементов и выводит на экран массив из сумм цифр каждого числа
+
+void FillArr (int[] array)
+{   int length = array.Length;
+        for (int i=0; i<length; i++){
+        array[i] = new Random().Next(0,10000);}
+}
+void PrintArray (int[] arr)
+{   int count = arr.Length;
+    int pos=0;
+    while (pos<count)
+        {Console.Write($"{arr[pos]} ");
+        pos++;}
+}
+int[] mass1 = new int[8];
+Console.WriteLine("массив из 8 чисел:");
+FillArr(mass1);
+PrintArray(mass1);
+Console.WriteLine();
+
+int Sum(int num){
+int sum=0;
+int temp=0;
+int ostatok=num;
+    while (ostatok != 0) {
+        temp = ostatok%10;
+        sum = sum + temp;
+        ostatok = ostatok/10;
+    }
+return sum;
+}
+Console.WriteLine("массив из сумм цифр каждого числа первого массива:");
+int[] mass2 = new int[8];
+    for (int ind=0; ind<mass2.Length; ind++){
+        mass2[ind]=Sum(mass1[ind]);}
+PrintArray(mass2);
+*/
+
+// Дополнительно
+// 1. Преобразовать массив так, чтобы сначала шли нулевые элементы, а затем все остальные
+
+void FillArr (int[] array)
+{   int length = array.Length;
+        for (int i=0; i<length; i++){
+        array[i] = new Random().Next(0,100);}
+}
+void PrintArray (int[] arr)
+{   int count = arr.Length;
+    int pos=0;
+    while (pos<count)
+        {Console.Write($"{arr[pos]} ");
+        pos++;}
+}
+Console.WriteLine("Введите число элементов массива");
+int len = int.Parse(Console.ReadLine());
+int[] massiv = new int [len];
+
+Console.WriteLine("Введите массив столбцом");
+FillArr(massiv);
+
+int[] massiv2 = new int [len];
+    int index = 1;
+    int temp = 0;
+    int j=0;
+    massiv2[0]=massiv[0];
+    while (index<len){
+        if (massiv[index] == 0) 
+            {   while (j<len) {
+                if (massiv[j] != 0) {
+                    temp = massiv[j];
+                    massiv[j]=0; 
+                    massiv[index]=temp}
+                else j++;}            }
+        index++;}
+
+Console.WriteLine();
+Console.WriteLine("исходный массив:");
+
+PrintArray(massiv);
+/*
+int[] massiv2 = new int [len];
+    for (int j=0; j<kol0; j++){
+        massiv2[j]=0;
+    }
+    for (int i=kol0; i<massiv2.Length; i++){
+        if (massiv[i-kol0] != 0) massiv2[i]=massiv[i];
+} */
+Console.WriteLine();
+Console.WriteLine("преобразованный массив:");
+PrintArray(massiv2);
